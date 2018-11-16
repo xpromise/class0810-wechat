efc8afda6223c38c2d86d272714da4668068bb36
xiongjian <webjsforyou@gmail.com>
2018-11-16 16:36:00 +0800

    接受用户发送消息和返回简单的响应

--- day01/01.验证服务器有效性/app.js
+++ day01/01.验证服务器有效性/app.js
@@ -35,14 +35,13 @@ app.use((req, res, next) => {
   //获取请求参数
   const {signature, echostr, timestamp, nonce} = req.query;
   const {token} = config;
-  
   // - 将参数签名加密的三个参数（timestamp、nonce、token）组合在一起，按照字典序排序
-  const arr = [timestamp, nonce, token].sort();
-  console.log(arr); // [ '1542350582', '477910604', 'atguiguHTML0810' ]
+  // const arr = [timestamp, nonce, token].sort();
+  // console.log(arr); // [ '1542350582', '477910604', 'atguiguHTML0810' ]
   // - 将排序后的参数拼接在一起，进行sha1加密
-  const str = sha1(arr.join(''));
-  console.log(str); // e131be1e59914a6f5eb0f254a59e09079c871fa8
-  // const str = sha1([timestamp, nonce, token].sort().join(''));
+  // const str = sha1(arr.join(''));
+  // console.log(str); // e131be1e59914a6f5eb0f254a59e09079c871fa8
+  const str = sha1([timestamp, nonce, token].sort().join(''));
   // - 加密后的到的就是微信签名，将其与微信发送过来的微信签名对比，
   if (signature === str) {
     //说明消息来自于微信服务器
@@ -51,6 +50,7 @@ app.use((req, res, next) => {
     //说明消息不来自于微信服务器
     res.end('error');
   }
+  
   
 })
 
---
+++ day01/02.接受用户发送的消息/app.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const sha1 = require('sha1');
+
+const {getUserDataAsync, parseXMLDataAsync, formatMessage} = require('./utils/tools');
+
+const app = express();
+/*
+  1. 搭建开发者服务器, 使用中间件接受请求
+  2. 默认localhost:3000访问本地服务器， 需要一个互联网能够访问的域名地址
+    借助ngrok工具，能将本地地址映射为互联网能访问的域名地址
+  3. 测试号管理页面填写服务器配置：
+    url：通过ngrok映射的地址   http://3389687c.ngrok.io
+    token：参与微信签名加密的参数， 自己定义，尽量复杂
+  4. 验证微信服务器有效性
+    目的：验证消息来自于微信服务器， 同时返回一个特定参数给微信服务器（告诉微信服务器我这里准备ok）
+    
+    - 将参数签名加密的三个参数（timestamp、nonce、token）组合在一起，按照字典序排序
+    - 将排序后的参数拼接在一起，进行sha1加密
+    - 加密后的到的就是微信签名，将其与微信发送过来的微信签名对比，
+      - 如果一样，说明消息来自于微信服务器，返回echostr给微信服务器
+      - 如果不一样，说明消息不是微信服务器发送过来的，返回error
+    
+ */
+const config = {
+  appID: 'wxc8e92f7ab70fbca0',
+  appsecret: 'b4054e90b75787c78e0af50bf7fc3e87',
+  token: 'atguiguHTML0810'
+}
+
+app.use(async (req, res, next) => {
+  console.log(req.query);
+  //获取请求参数
+  const {signature, echostr, timestamp, nonce} = req.query;
+  const {token} = config;
+  const str = sha1([timestamp, nonce, token].sort().join(''));
+  /*
+    微信服务器会发送两种类型的消息给开发者
+      1. GET 验证服务器有效性逻辑
+      2. POST 转发用户消息
+   */
+  if (req.method === 'GET') {
+    // 验证服务器有效性逻辑
+    if (signature === str) {
+      //说明消息来自于微信服务器
+      res.end(echostr);
+    } else {
+      //说明消息不来自于微信服务器
+      res.end('error');
+    }
+  } else if (req.method === 'POST') {
+    // 转发用户消息
+    //接受微信服务器转发用户消息
+    //验证消息来自于微信服务器
+    if (signature !== str) {
+      res.end('error');
+      return;
+    }
+    //用户发送的消息在请求体
+    const xmlData = await getUserDataAsync(req);
+    console.log(xmlData);
+    /*
+    <xml>
+      <ToUserName><![CDATA[gh_4fe7faab4d6c]]></ToUserName>    开发者的微信号
+      <FromUserName><![CDATA[oAsoR1iP-_D3LZIwNCnK8BFotmJc]]></FromUserName>  微信用户openid
+      <CreateTime>1542355200</CreateTime>   发送消息的时间戳
+      <MsgType><![CDATA[text]]></MsgType>   消息类型
+      <Content><![CDATA[111]]></Content>    消息的具体内容
+      <MsgId>6624365143243452763</MsgId>    消息id，微信服务器会默认保存3天微信用户发送的消息，在此期间内通过这id就能找到当前消息
+    </xml>
+     */
+    //将用户发送过来的xml数据解析为js对象
+    const jsData = await parseXMLDataAsync(xmlData);
+    console.log(jsData);
+    /*
+    {
+      xml:
+       { ToUserName: [ 'gh_4fe7faab4d6c' ],
+         FromUserName: [ 'oAsoR1iP-_D3LZIwNCnK8BFotmJc' ],
+         CreateTime: [ '1542355988' ],
+         MsgType: [ 'text' ],
+         Content: [ '222' ],
+         MsgId: [ '6624368527677682013' ]
+      }
+    }
+     */
+    //格式化数据
+    const message = formatMessage(jsData);
+    console.log(message);
+    /*
+    { ToUserName: 'gh_4fe7faab4d6c',
+      FromUserName: 'oAsoR1iP-_D3LZIwNCnK8BFotmJc',
+      CreateTime: '1542356422',
+      MsgType: 'text',
+      Content: '333',
+      MsgId: '6624370391693488478' }
+     */
+    //初始化一个消息文本
+    let content = '你在说什么，我听不懂~';
+    
+    //判断用户发送消息的内容，根据内容返回特定的响应
+    if (message.Content === '1') {  //全匹配
+      content = '大吉大利，今晚吃鸡';
+    } else if (message.Content === '2') {
+      content = '落地成盒';
+    } else if (message.Content.includes('爱')) {  //半匹配
+      content = '我爱你~';
+    }
+    
+    //返回xml消息给微信服务器
+    //微信官网提供的xml数据有问题，有多余的空格，要手动去除，不去除会报错
+    let replyMessage = `<xml>
+      <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+      <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+      <CreateTime>${Date.now()}</CreateTime>
+      <MsgType><![CDATA[text]]></MsgType>
+      <Content><![CDATA[${content}]]></Content>
+      </xml>`;
+    
+    /*
+      注意：微信服务器当没有接收到开发者服务器响应时，默认会请求3次开发者服务器，就会导致接口被调用多次
+      解决：提前返回一个值给微信服务器  res.end('');
+     */
+    res.send(replyMessage);
+    
+  } else {
+    res.end('error');
+  }
+  
+})
+
+
+app.listen(3000, err => {
+  if (!err) console.log('服务器启动成功了~');
+  else console.log(err);
+})
